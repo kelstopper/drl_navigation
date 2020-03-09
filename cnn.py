32badcc4180b5a70299dfd8d3ba2208d2e21cfd4
Kel Stopper <kelstopper@gmail.com>
2020-03-09 15:13:43 -0400

    update hyper parameters

--- cnn.py
+++ cnn.py
@@ -31,12 +31,12 @@ print('States have shape:', state.shape)
 
 from agent.dqn_agent import Agent, LearningMethod, NetworkType
 
-BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 128         # minibatch size
+BUFFER_SIZE = int(1e7)  # replay buffer size
+BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 1e-4              # for soft update of target parameters
+TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate 
-UPDATE_EVERY = 10        # how often to update the network
+UPDATE_EVERY = 4        # how often to update the network
 
 #initialize the agent
 agent = Agent(

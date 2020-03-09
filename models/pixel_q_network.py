32da2322f8d7c1e67a78ad3ab280cbae6ddcb77f
Kel Stopper <kelstopper@gmail.com>
2020-03-09 15:54:01 -0400

    change the conv layers

--- models/pixel_q_network.py
+++ models/pixel_q_network.py
@@ -19,25 +19,26 @@ class PixelQNetwork(nn.Module):
         self.seed = torch.manual_seed(seed)
 
         self.cnn = nn.Sequential(
-            nn.Conv2d(state_size, 4, kernel_size=2),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(4, 8, kernel_size=2),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(8, 16, kernel_size=2),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.Conv2d(3, state_size, kernel_size=2),
+            nn.LeakyReLU(0.02),
+            nn.Conv2d(state_size, state_size * 2, kernel_size=2),
+            nn.LeakyReLU(0.02),
+            nn.Conv2d(state_size * 2, state_size * 4, kernel_size=2),
+            nn.LeakyReLU(0.02),
+            nn.Conv2d(state_size * 4, state_size * 8, kernel_size=2),
+            nn.LeakyReLU(0.02),
+            nn.Conv2d(state_size * 8, state_size * 16, kernel_size=2),
+            nn.LeakyReLU(0.02),
         )
 
         self.fc = nn.Sequential(
-            nn.Linear(1296, 648),
+            nn.Linear(299568, 149784),
             nn.ReLU(),
-            nn.Linear(648, 324),
+            nn.Linear(149784, 74892),
             nn.ReLU(),
-            nn.Linear(324, 162),
+            nn.Linear(74892, 37446),
             nn.ReLU(),
-            nn.Linear(162, action_size)
+            nn.Linear(18723, action_size)
         )
 
     def forward(self, state):

be3a1c3cbd35247d45186873e92f1f2c0880d9ed
Kel Stopper <kelstopper@gmail.com>
2020-03-09 22:15:12 -0400

    update cnn layer

--- models/pixel_q_network.py
+++ models/pixel_q_network.py
@@ -19,26 +19,30 @@ class PixelQNetwork(nn.Module):
         self.seed = torch.manual_seed(seed)
 
         self.cnn = nn.Sequential(
-            nn.Conv2d(3, state_size, kernel_size=2),
+            nn.Conv2d(3, 84, kernel_size=3, stride=2),
             nn.LeakyReLU(0.02),
-            nn.Conv2d(state_size, state_size * 2, kernel_size=2),
+            nn.Conv2d(84, 168, kernel_size=3, stride=2),
+            nn.BatchNorm2d(168),
             nn.LeakyReLU(0.02),
-            nn.Conv2d(state_size * 2, state_size * 4, kernel_size=2),
+            nn.Conv2d(168, 336, kernel_size=3, stride=2),
+            nn.BatchNorm2d(336),
             nn.LeakyReLU(0.02),
-            nn.Conv2d(state_size * 4, state_size * 8, kernel_size=2),
+            nn.Conv2d(336, 672, kernel_size=3, stride=2),
+            nn.BatchNorm2d(672),
             nn.LeakyReLU(0.02),
-            nn.Conv2d(state_size * 8, state_size * 16, kernel_size=2),
+            nn.Conv2d(672, 1344, kernel_size=3, stride=2),
+            nn.BatchNorm2d(1344),
             nn.LeakyReLU(0.02),
         )
 
         self.fc = nn.Sequential(
-            nn.Linear(299568, 149784),
+            nn.Linear(1344, 1344),
             nn.ReLU(),
-            nn.Linear(149784, 74892),
+            nn.Linear(1344, 672),
             nn.ReLU(),
-            nn.Linear(74892, 37446),
+            nn.Linear(672, 336),
             nn.ReLU(),
-            nn.Linear(18723, action_size)
+            nn.Linear(336, action_size)
         )
 
     def forward(self, state):
